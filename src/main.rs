b5e1f0a0009fd0e89ce3711ab9307c55b5dd932e
Adam Keim <adam.frusa@gmail.com>
2021-02-22 23:39:37 -0700

    Getting up to surface normals

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,72 @@
-fn main() {
-    let width = 256;
-    let height = 256;
+use glam::Vec3A;
+use ray::Ray;
 
-    println!("P3\n{} {}\n255", width, height);
-    for j in height..0 {
-        println!("{}", j);
+mod ray;
+
+fn main() {
+    //Image Config
+    let aspect_ratio = 16.0 / 9.0;
+    let n_x: u32 = 1000; //This is where we change resolution
+    let n_y = (n_x as f32 / aspect_ratio) as u32;
+
+    // Camera Config
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec3A::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3A::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3A::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3A::new(0.0, 0.0, focal_length);
+
+    println!("P3\n{} {}\n255", n_x, n_y);
+    for j in (0..n_y).rev() {
+        eprintln!("{}", j);
+        for i in 0..n_x {
+            let u = i as f32 / n_x as f32;
+            let v = j as f32 / n_y as f32;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
+            );
+            let pixel_color = ray_color(r);
+            write_color(pixel_color);
+        }
     }
+}
+
+fn ray_color(r: Ray) -> Vec3A {
+    let t = hit_sphere(Vec3A::new(0.0, 0.0, -1.0), 0.5, &r);
+    if t > 0.0 {
+        let N = (r.at(t) - Vec3A::new(0.0, 0.0, -1.0)).normalize();
+        return 0.5 * Vec3A::new(N.x + 1.0, N.y + 1.0, N.z + 1.0);
+    }
+    let unit_direction = r.direction().normalize();
+    let t = 0.5 * (unit_direction.y + 1.0);
+
+    (1.0 - t) * Vec3A::new(1.0, 1.0, 1.0) + t * Vec3A::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Vec3A, radius: f32, ray: &Ray) -> f32 {
+    let oc: Vec3A = ray.origin() - center;
+    let a = ray.direction().length_squared();
+    let half_b = oc.dot(ray.direction());
+    let c = oc.length_squared() - radius * radius;
+    let disc = half_b * half_b - a * c;
+
+    if disc < 0.0 {
+        -1.0
+    } else {
+        (-half_b - disc.sqrt()) / a
+    }
+}
+
+fn write_color(color: Vec3A) {
+    println!(
+        "{} {} {}",
+        (color.x * 255.99) as u32,
+        (color.y * 255.99) as u32,
+        (color.z * 255.99) as u32
+    );
 }
---
+++ src/ray.rs
@@ -0,0 +1,21 @@
+use glam::Vec3A;
+type Point3 = Vec3A;
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3A,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3A) -> Ray {
+        Ray { origin, direction }
+    }
+    pub fn origin(&self) -> Point3 {
+        self.origin
+    }
+    pub fn direction(&self) -> Vec3A {
+        self.direction
+    }
+    pub fn at(&self, t: f32) -> Point3 {
+        self.origin + self.direction * t
+    }
+}
